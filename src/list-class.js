3fbe927c2b2f10e4537222ac3cc4e71e23f46ed5
sayed94h <skazimi951@yahoo.com>
2020-09-17 14:44:49 +0200

    TodoList-container part is finished

--- src/list-class.js
+++ src/list-class.js
@@ -1,33 +1,45 @@
-'use strict';
+"use strict";
 
-import { logger } from '../lib/logger.js';
+import { logger } from "../lib/logger.js";
 
 // This list class is here for example.
 //  delete it!  your project does not need to be a list
 
 export class List {
+	state = {
+		name: "",
+	};
 
-  state = {
-    name: ''
-  };
+	constructor(name) {
+		this.state.name = name;
+	}
 
-  constructor(name) {
-    this.state.name = name;
-  };
+	printState() {
+		console.log(this.state.name);
 
-  printState() {
-    console.log(this.state.name);
+		logger.push({
+			action: `click button`,
+			name: this.state.name,
+		});
+	}
 
-    logger.push({
-      action: `click button`,
-      name: this.state.name
-    });
-  };
+	render() {
+		let listBox = document.createElement("div");
+		listBox.id = "list-box";
+		let todoTitle = document.createElement("h2");
+		todoTitle.textContent = this.state.name;
+		let inputItems = document.createElement("input");
+		inputItems.type = "text";
+		inputItems.name = "inputItems";
+		inputItems.placeholder = "Enter Tasks To Do";
 
-  render() {
-    const buttonEl = document.createElement('button');
-    buttonEl.innerHTML = this.state.name;
-    buttonEl.addEventListener('click', this.printState.bind(this));
-    return buttonEl;
-  };
-};
+		const addItems = document.createElement("button");
+		addItems.textContent = "Add";
+		addItems.addEventListener("click", this.printState.bind(this));
+		listBox.appendChild(todoTitle);
+		listBox.appendChild(inputItems);
+		listBox.appendChild(addItems);
+
+		return listBox;
+	}
+}
