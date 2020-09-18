cb39ce538546441480c76c97e519b6b129b3fdde
EMEA\burtear <razvan.burtea@dematic.com>
2020-09-18 15:13:42 +0200

    add items function

--- src/list-class.js
+++ src/list-class.js
@@ -22,6 +22,14 @@ export class List {
 			name: this.state.name,
 		});
 	}
+	addItems() {
+		console.log(this.state.name);
+
+		logger.push({
+			action: `click button`,
+			name: this.state.name,
+		});
+	}
 
 	render() {
 		let listBox = document.createElement("div");
@@ -35,11 +43,85 @@ export class List {
 
 		const addItems = document.createElement("button");
 		addItems.textContent = "Add";
-		addItems.addEventListener("click", this.printState.bind(this));
+		//addItems.addEventListener("click", this.printState.bind(this));
+		addItems.addEventListener("click", this.addItems.bind(this));
 		listBox.appendChild(todoTitle);
 		listBox.appendChild(inputItems);
 		listBox.appendChild(addItems);
+		
 
 		return listBox;
 	}
+
+	
+	
+	// Function addItems
+	addItems  () {
+		debugger;
+	
+		let inputID = `${this.state.name}HaveTodo`;
+		let itemValue = document.getElementById(inputID).value;
+	
+		if (itemValue === "") {
+		  alert("Enter something to add in your Todo List!");
+		  return;
+		}
+	
+		this.state.allTodo.push({
+		  items: itemValue,
+		  completed: false,
+		});
+		this.displayItems();
+		document.getElementById(inputID).value = "";
+		logger.push({
+		  action: "Add Items to the list",
+		  stateName: this.state.name,
+		  state: this.state,
+		});
+	  }
+	
+	  displayItems () {
+		debugger;
+	
+		let itemLists = document.getElementById(`${this.state.name}ol`);
+		itemLists.innerHTML = "";
+	
+		this.state.allTodo.forEach(function (todo, position) {
+		  let todoItems = document.createElement("li");
+		  todoItems.id = position;
+		  checkItem = document.createElement('input'); 
+		  checkItem.type = "checkbox";
+		  
+		  checkItem.className = "checkItem";
+		
+		  checkItem.addEventListener("click", this.toggleComplete.bind(this, todo));
+	
+		  if(todo.completed === true){
+			checkItem.checked = true;
+		  } else { checkItem.checked = false;}
+	
+		  let spanEl = document.createElement('span');
+		  spanEl.innerHTML = todo.items;
+		  todoItems.appendChild(checkItem);
+		  todoItems.appendChild(spanEl);
+	
+		  //delete button
+		  var deleteButton = document.createElement("button");
+		  deleteButton.innerHTML = `<i class="fa fa-trash-o" style= "font-size:1.7em;color:red; font-weight: bold;"></i>`;
+		  deleteButton.style.color = "red";
+	
+		  deleteButton.style.fontWeight = "bold";
+		  
+		  deleteButton.className = "deleteItemList";
+	
+		  todoItems.appendChild(deleteButton);
+		  deleteButton.addEventListener(
+			"click",
+			this.deleteItems.bind(this, position)
+		  );
+	
+	
+		  itemLists.appendChild(todoItems);
+		}, this);
+	  }
 }
